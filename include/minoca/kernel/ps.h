minoca/os

commit 7da5e2a6d79e9c406b0853c05b892fcd1e50d222

Moved PROCESS_ENVIRONMENT block to the user stack.
Before this change we would just map some region of memory and put the
environment block there. This won't work so well for emacs undump, as the
mapped region is usually a very low address, and undump would end up
saving that block. Additionally, applications are probably used to
RLIMIT_STACK capping both the stack and the arguments. A mild side benefit
is the environment doesn't need to round up to a page, as the stack
simply starts right beyond it.

Most of the work in this change came from moving things around to satisfy
the conditions that 1) when the user process environment block is created
the user stack must already be set up, and 2) The stack pointer register
needs to point just beyond the environment block in this case.

Also removed an unused THREAD_FLAG_ADD_REFERENCE.

include/minoca/kernel/ps.h
@@ -421,10 +421,9 @@ Author:
 //
 
 #define THREAD_FLAG_USER_MODE       0x0001
-#define THREAD_FLAG_ADD_REFERENCE   0x0002
-#define THREAD_FLAG_IN_SYSTEM_CALL  0x0004
-#define THREAD_FLAG_SINGLE_STEP     0x0008
-#define THREAD_FLAG_FREE_USER_STACK 0x0010
+#define THREAD_FLAG_IN_SYSTEM_CALL  0x0002
+#define THREAD_FLAG_SINGLE_STEP     0x0004
+#define THREAD_FLAG_FREE_USER_STACK 0x0008
 
 //
 // Define thread FPU flags.
@@ -438,8 +437,7 @@ Author:
 // also the set of flags that will propagate when a thread is copied.
 //
 
-#define THREAD_FLAG_CREATION_MASK \
-    (THREAD_FLAG_USER_MODE | THREAD_FLAG_ADD_REFERENCE)
+#define THREAD_FLAG_CREATION_MASK (THREAD_FLAG_USER_MODE)
 
 #define DEFAULT_USER_STACK_SIZE (8 * _1MB)
 #define DEFAULT_KERNEL_STACK_SIZE 0x3000
@@ -1873,6 +1871,10 @@ Members:
         this is a user mode address, this will also be set as the thread ID
         pointer value for the thread.
 
+    Environment - Stores an optional kernel mode pointer to an environment to
+        copy over to the stack of the created thread. This is only used when
+        creating a process directly from kernel mode.
+
 --*/
 
 typedef struct _THREAD_CREATION_PARAMETERS {
@@ -1886,6 +1888,7 @@ typedef struct _THREAD_CREATION_PARAMETERS {
     ULONG Flags;
     PVOID ThreadPointer;
     PTHREAD_ID ThreadIdPointer;
+    PPROCESS_ENVIRONMENT Environment;
 } THREAD_CREATION_PARAMETERS, *PTHREAD_CREATION_PARAMETERS;
 
 //
@@ -3765,7 +3768,7 @@ PsCopyEnvironment (
     PPROCESS_ENVIRONMENT Source,
     PPROCESS_ENVIRONMENT *Destination,
     BOOL FromUserMode,
-    BOOL ToUserMode
+    PKTHREAD DestinationThread
     );
 
 /*++
@@ -3784,9 +3787,9 @@ Arguments:
     FromUserMode - Supplies a boolean indicating whether the environment exists
         in user mode or not.
 
-    ToUserMode - Supplies a boolean indicating whether the environment should
-        be copied into user space. If this variable is set, the source
-        environment must come from kernel mode.
+    DestinationThread - Supplies an optional pointer to the user mode thread
+        to copy the environment into. Supply NULL to copy the environment to
+        a new kernel mode buffer.
 
 Return Value:
 

kernel/ps/env.c
@@ -66,7 +66,7 @@ PsCopyEnvironment (
     PPROCESS_ENVIRONMENT Source,
     PPROCESS_ENVIRONMENT *Destination,
     BOOL FromUserMode,
-    BOOL ToUserMode
+    PKTHREAD DestinationThread
     )
 
 /*++
@@ -85,9 +85,9 @@ Arguments:
     FromUserMode - Supplies a boolean indicating whether the environment exists
         in user mode or not.
 
-    ToUserMode - Supplies a boolean indicating whether the environment should
-        be copied into user space. If this variable is set, the source
-        environment must come from kernel mode.
+    DestinationThread - Supplies an optional pointer to the user mode thread
+        to copy the environment into. Supply NULL to copy the environment to
+        a new kernel mode buffer.
 
 Return Value:
 
@@ -104,7 +104,6 @@ Return Value:
     ULONG ElementCount;
     ULONG ElementIndex;
     UINTN EnvironmentBufferLength;
-    ULONG Flags;
     UINTN ImageNameLength;
     PPROCESS_ENVIRONMENT NewEnvironment;
     UINTN Offset;
@@ -116,7 +115,7 @@ Return Value:
     // Environments cannot be copied directly from user mode to user mode.
     //
 
-    ASSERT((FromUserMode == FALSE) || (ToUserMode == FALSE));
+    ASSERT((FromUserMode == FALSE) || (DestinationThread == NULL));
 
     NewEnvironment = NULL;
     Process = PsGetCurrentProcess();
@@ -126,7 +125,7 @@ Return Value:
     // process.
     //
 
-    ASSERT(((FromUserMode == FALSE) && (ToUserMode == FALSE)) ||
+    ASSERT(((FromUserMode == FALSE) && (DestinationThread == NULL)) ||
            (Process != PsGetKernelProcess()));
 
     //
@@ -164,7 +163,7 @@ Return Value:
                      EnvironmentBufferLength +
                      sizeof(PROCESS_START_DATA);
 
-    if (ToUserMode != FALSE) {
+    if (DestinationThread != NULL) {
         AllocationSize += sizeof(PROCESS_START_DATA);
     }
 
@@ -173,27 +172,37 @@ Return Value:
         goto CopyEnvironmentEnd;
     }
 
-    if (ToUserMode != FALSE) {
-        Flags = IMAGE_SECTION_READABLE | IMAGE_SECTION_WRITABLE;
-        Status = MmMapFileSection(INVALID_HANDLE,
-                                  0,
-                                  AllocationSize,
-                                  Flags,
-                                  FALSE,
-                                  NULL,
-                                  AllocationStrategyAnyAddress,
-                                  (PVOID)(&NewEnvironment));
+    if (DestinationThread != NULL) {
 
-        if (!KSUCCESS(Status)) {
+        //
+        // If copying to user mode, then the user stack had better be set up,
+        // and this had better be the only thread (otherwise sudden unmappings
+        // could cause bad faults in kernel mode).
+        //
+
+        ASSERT((DestinationThread->UserStackSize != 0) &&
+               (DestinationThread->OwningProcess == Process) &&
+               (DestinationThread->ThreadParameter == NULL));
+
+        //
+        // Don't allow the environment to cover too much of the stack.
+        //
+
+        if (AllocationSize > DestinationThread->UserStackSize / 2) {
+            RtlDebugPrint("Environment too large!\n");
+            Status = STATUS_INSUFFICIENT_RESOURCES;
             goto CopyEnvironmentEnd;
         }
 
+        NewEnvironment = DestinationThread->UserStack +
+                         DestinationThread->UserStackSize -
+                         AllocationSize;
+
         //
-        // It's okay to directly write to user mode memory in this case
-        // because there is no user mode code running.
+        // Set the thread parameter to point at the environment.
         //
 
-        ASSERT(Process->ThreadCount == 1);
+        DestinationThread->ThreadParameter = NewEnvironment;
 
     } else {
         NewEnvironment = MmAllocatePagedPool(AllocationSize,
@@ -443,7 +452,7 @@ Return Value:
 
     ASSERT(AllocationSize >= sizeof(PROCESS_START_DATA));
 
-    if (ToUserMode != FALSE) {
+    if (DestinationThread != NULL) {
 
         ASSERT(Source->StartData != NULL);
 

kernel/ps/armv7/psarch.c
@@ -248,7 +248,8 @@ Return Value:
 VOID
 PspPrepareThreadForFirstRun (
     PKTHREAD Thread,
-    PTRAP_FRAME TrapFrame
+    PTRAP_FRAME TrapFrame,
+    BOOL ParameterIsStack
     )
 
 /*++
@@ -265,6 +266,9 @@ Arguments:
     TrapFrame - Supplies an optional pointer for the thread to restore on its
         first run.
 
+    ParameterIsStack - Supplies a boolean indicating whether the thread
+        parameter is the value that should be used as the initial stack pointer.
+
 Return Value:
 
     None.
@@ -277,7 +281,7 @@ Return Value:
     ULONG Flags;
     PULONG StackPointer;
     PTRAP_FRAME StackTrapFrame;
-    UINTN UserStackPointer;
+    PVOID UserStackPointer;
 
     //
     // Get the initial stack pointer, and word align it.
@@ -293,12 +297,24 @@ Return Value:
     if ((Thread->Flags & THREAD_FLAG_USER_MODE) != 0) {
         Flags |= ARM_MODE_USER;
         EntryPoint = (UINTN)Thread->ThreadRoutine;
-        UserStackPointer = (UINTN)Thread->UserStack + Thread->UserStackSize;
+
+        ASSERT((TrapFrame == NULL) || (ParameterIsStack == FALSE));
+
+        if (ParameterIsStack != FALSE) {
+            UserStackPointer = Thread->ThreadParameter;
+
+            ASSERT((UserStackPointer >= Thread->UserStack) &&
+                   (UserStackPointer <
+                    Thread->UserStack + Thread->UserStackSize));
+
+        } else {
+            UserStackPointer = Thread->UserStack + Thread->UserStackSize;
+        }
 
     } else {
         Flags |= ARM_MODE_SVC;
         EntryPoint = (UINTN)PspKernelThreadStart;
-        UserStackPointer = 0x66666666;
+        UserStackPointer = (PVOID)0x66666666;
     }
 
     if (((UINTN)EntryPoint & ARM_THUMB_BIT) != 0) {
@@ -360,9 +376,13 @@ Return Value:
 
     ULONG OldSvcLink;
     ULONG OldSvcStackPointer;
-    PUINTN UserStackPointer;
+    PVOID UserStackPointer;
 
-    UserStackPointer = Thread->UserStack + Thread->UserStackSize;
+    UserStackPointer = Thread->ThreadParameter;
+
+    ASSERT((UserStackPointer >= Thread->UserStack) &&
+           (UserStackPointer < Thread->UserStack + Thread->UserStackSize));
+
     OldSvcLink = TrapFrame->SvcLink;
     OldSvcStackPointer = TrapFrame->SvcSp;
     RtlZeroMemory(TrapFrame, sizeof(TRAP_FRAME));

kernel/ps/process.c
@@ -489,7 +489,6 @@ Return Value:
     PROCESS_START_DATA StartData;
     KSTATUS Status;
     PKTHREAD Thread;
-    PPROCESS_ENVIRONMENT UserEnvironment;
 
     ASSERT(SystemCallNumber == SystemCallExecuteImage);
 
@@ -520,7 +519,7 @@ Return Value:
     Status = PsCopyEnvironment(&(Parameters->Environment),
                                &NewEnvironment,
                                TRUE,
-                               FALSE);
+                               NULL);
 
     if (!KSUCCESS(Status)) {
         goto SysExecuteProcessEnd;
@@ -650,31 +649,11 @@ Return Value:
     Thread->ThreadRoutine = StartData.EntryPoint;
 
     //
-    // Create the user mode environment.
-    //
-
-    Status = PsCopyEnvironment(Process->Environment,
-                               &UserEnvironment,
-                               FALSE,
-                               TRUE);
-
-    if (!KSUCCESS(Status)) {
-        goto SysExecuteProcessEnd;
-    }
-
-    Process->Environment->StartData = NULL;
-
-    //
-    // Set the new environment as the thread parameter.
-    //
-
-    Thread->ThreadParameter = UserEnvironment;
-
-    //
     // Reset the thread in preparation for execution.
     //
 
     Status = PspResetThread(Thread, TrapFrame);
+    Process->Environment->StartData = NULL;
     if (!KSUCCESS(Status)) {
         goto SysExecuteProcessEnd;
     }
@@ -2048,7 +2027,7 @@ Return Value:
             Status = PsCopyEnvironment(SourceEnvironment,
                                        &Environment,
                                        FALSE,
-                                       FALSE);
+                                       NULL);
 
         } else {
             Status = PsCreateEnvironment(CommandLine,
@@ -3407,7 +3386,6 @@ Return Value:
     PROCESS_START_DATA StartData;
     KSTATUS Status;
     THREAD_CREATION_PARAMETERS ThreadParameters;
-    PPROCESS_ENVIRONMENT UserEnvironment;
 
     Process = PsGetCurrentProcess();
 
@@ -3436,21 +3414,6 @@ Return Value:
     Process->Environment->StartData = &StartData;
 
     //
-    // Create the user mode environment.
-    //
-
-    Status = PsCopyEnvironment(Process->Environment,
-                               &UserEnvironment,
-                               FALSE,
-                               TRUE);
-
-    if (!KSUCCESS(Status)) {
-        goto LoaderThreadEnd;
-    }
-
-    Process->Environment->StartData = NULL;
-
-    //
     // Kick off the primary usermode thread.
     //
 
@@ -3458,9 +3421,10 @@ Return Value:
     ThreadParameters.Name = "MainThread";
     ThreadParameters.NameSize = sizeof("MainThread");
     ThreadParameters.ThreadRoutine = StartData.EntryPoint;
-    ThreadParameters.Parameter = UserEnvironment;
+    ThreadParameters.Environment = Process->Environment;
     ThreadParameters.Flags = THREAD_FLAG_USER_MODE;
     Status = PsCreateThread(&ThreadParameters);
+    Process->Environment->StartData = NULL;
     if (!KSUCCESS(Status)) {
         goto LoaderThreadEnd;
     }

kernel/ps/psp.h
@@ -839,7 +839,8 @@ Return Value:
 VOID
 PspPrepareThreadForFirstRun (
     PKTHREAD Thread,
-    PTRAP_FRAME TrapFrame
+    PTRAP_FRAME TrapFrame,
+    BOOL ParameterIsStack
     );
 
 /*++
@@ -855,6 +856,9 @@ Arguments:
 
     TrapFrame - Supplies an optional pointer for the thread to restore on its
         first run.
+
+    ParameterIsStack - Supplies a boolean indicating whether the thread
+        parameter is the value that should be used as the initial stack pointer.
 
 Return Value:
 

kernel/ps/thread.c
@@ -212,7 +212,9 @@ Return Value:
     PKTHREAD CurrentThread;
     UINTN KernelStackSize;
     PKTHREAD NewThread;
+    BOOL ParameterIsStack;
     KSTATUS Status;
+    PPROCESS_ENVIRONMENT UserEnvironment;
 
     KernelStackSize = 0;
     if ((Parameters->Flags & THREAD_FLAG_USER_MODE) == 0) {
@@ -263,6 +265,7 @@ Return Value:
     // Create the user mode stack if needed.
     //
 
+    ParameterIsStack = FALSE;
     if ((Parameters->Flags & THREAD_FLAG_USER_MODE) != 0) {
         if (Parameters->UserStack == NULL) {
             NewThread->Flags |= THREAD_FLAG_FREE_USER_STACK;
@@ -292,9 +295,25 @@ Return Value:
         RtlCopyMemory(&(NewThread->BlockedSignals),
                       &(CurrentThread->BlockedSignals),
                       sizeof(SIGNAL_SET));
+
+        //
+        // Set up the environment if there is one.
+        //
+
+        if (Parameters->Environment != NULL) {
+            ParameterIsStack = TRUE;
+            Status = PsCopyEnvironment(Parameters->Environment,
+                                       &UserEnvironment,
+                                       FALSE,
+                                       NewThread);
+
+            if (!KSUCCESS(Status)) {
+                goto CreateThreadEnd;
+            }
+        }
     }
 
-    PspPrepareThreadForFirstRun(NewThread, NULL);
+    PspPrepareThreadForFirstRun(NewThread, NULL, ParameterIsStack);
 
     //
     // Insert the thread onto the ready list.
@@ -309,9 +328,6 @@ CreateThreadEnd:
             PspSetThreadUserStackSize(NewThread, 0);
             PspDestroyCredentials(NewThread);
             ObReleaseReference(NewThread);
-            if ((Parameters->Flags & THREAD_FLAG_ADD_REFERENCE) != 0) {
-                ObReleaseReference(NewThread);
-            }
         }
     }
 
@@ -1066,14 +1082,6 @@ Return Value:
     PKTHREAD NewThread;
     KSTATUS Status;
 
-    //
-    // The add reference flag had better not be set, as it's unlikely that the
-    // kernel entity that set that flag is now aware that it has another
-    // release to do on this newly forked thread.
-    //
-
-    ASSERT((Thread->Flags & THREAD_FLAG_ADD_REFERENCE) == 0);
-
     NewThread = PspCreateThread(DestinationProcess,
                                 Thread->KernelStackSize,
                                 Thread->ThreadRoutine,
@@ -1108,7 +1116,7 @@ Return Value:
     NewThread->BlockedSignals = Thread->BlockedSignals;
     NewThread->UserStack = Thread->UserStack;
     NewThread->UserStackSize = Thread->UserStackSize;
-    PspPrepareThreadForFirstRun(NewThread, TrapFrame);
+    PspPrepareThreadForFirstRun(NewThread, TrapFrame, FALSE);
     NewThread->ThreadPointer = Thread->ThreadPointer;
     NewThread->ThreadIdPointer = Thread->ThreadIdPointer;
 
@@ -1123,7 +1131,6 @@ CloneThreadEnd:
     if (!KSUCCESS(Status)) {
         if (NewThread != NULL) {
 
-            ASSERT((NewThread->Flags & THREAD_FLAG_ADD_REFERENCE) == 0);
             ASSERT(NewThread->SupplementaryGroups == NULL);
 
             ObReleaseReference(NewThread);
@@ -1166,6 +1173,7 @@ Return Value:
 {
 
     KSTATUS Status;
+    PPROCESS_ENVIRONMENT UserEnvironment;
 
     //
     // Create the user mode stack.
@@ -1177,6 +1185,16 @@ Return Value:
     Status = PspSetThreadUserStackSize(
                                    Thread,
                                    Thread->Limits[ResourceLimitStack].Current);
+
+    if (!KSUCCESS(Status)) {
+        goto CreateThreadEnd;
+    }
+
+    Thread->ThreadParameter = NULL;
+    Status = PsCopyEnvironment(Thread->OwningProcess->Environment,
+                               &UserEnvironment,
+                               FALSE,
+                               Thread);
 
     if (!KSUCCESS(Status)) {
         goto CreateThreadEnd;
@@ -1514,16 +1532,6 @@ Return Value:
     NewThread->ThreadPointer = PsInitialThreadPointer;
 
     //
-    // Add an extra reference if desired. This is used so that the creator can
-    // wait on the thread object without having to worry about racing with the
-    // thread exiting.
-    //
-
-    if ((Flags & THREAD_FLAG_ADD_REFERENCE) != 0) {
-        ObAddReference(NewThread);
-    }
-
-    //
     // Allocate a kernel stack.
     //
 
@@ -1613,10 +1621,6 @@ CreateThreadEnd:
     if (!KSUCCESS(Status)) {
         if (NewThread != NULL) {
             ObReleaseReference(NewThread);
-            if ((Flags & THREAD_FLAG_ADD_REFERENCE) != 0) {
-                ObReleaseReference(NewThread);
-            }
-
             NewThread = NULL;
         }
     }

kernel/ps/x86/psarch.c
@@ -262,7 +262,8 @@ Return Value:
 VOID
 PspPrepareThreadForFirstRun (
     PKTHREAD Thread,
-    PTRAP_FRAME TrapFrame
+    PTRAP_FRAME TrapFrame,
+    BOOL ParameterIsStack
     )
 
 /*++
@@ -278,6 +279,9 @@ Arguments:
 
     TrapFrame - Supplies an optional pointer for the thread to restore on its
         first run.
+
+    ParameterIsStack - Supplies a boolean indicating whether the thread
+        parameter is the value that should be used as the initial stack pointer.
 
 Return Value:
 
@@ -308,15 +312,26 @@ Return Value:
 
     if ((Thread->Flags & THREAD_FLAG_USER_MODE) != 0) {
 
+        ASSERT((TrapFrame == NULL) || (ParameterIsStack == FALSE));
+
         //
         // Set up the values on the user mode stack. Push the parameter and a
         // dummy return address.
         //
 
-        UserStackPointer = Thread->UserStack + Thread->UserStackSize -
-                           sizeof(PVOID);
-
         if (TrapFrame == NULL) {
+            if (ParameterIsStack != FALSE) {
+                UserStackPointer = Thread->ThreadParameter - sizeof(PVOID);
+
+                ASSERT(((PVOID)UserStackPointer >= Thread->UserStack) &&
+                       ((PVOID)UserStackPointer <
+                        Thread->UserStack + Thread->UserStackSize));
+
+            } else {
+                UserStackPointer = Thread->UserStack + Thread->UserStackSize -
+                                   sizeof(PVOID);
+            }
+
             MmUserWrite(UserStackPointer, (UINTN)Thread->ThreadParameter);
             UserStackPointer -= 1;
             MmUserWrite(UserStackPointer, 0);
@@ -412,8 +427,11 @@ Return Value:
     PUINTN UserStackPointer;
 
     Thread->ThreadPointer = PsInitialThreadPointer;
-    UserStackPointer = Thread->UserStack + Thread->UserStackSize -
-                       sizeof(PVOID);
+    UserStackPointer = Thread->ThreadParameter - sizeof(PVOID);
+
+    ASSERT(((PVOID)UserStackPointer >= Thread->UserStack) &&
+           ((PVOID)UserStackPointer <
+            Thread->UserStack + Thread->UserStackSize));
 
     MmUserWrite(UserStackPointer, (UINTN)(Thread->ThreadParameter));
     UserStackPointer -= 1;

commit 29e69259998e9bb5c94fae931816f6aa19d34421

Add stack alignment broken by 7da5e2a.
When moving the PROCESS_ENVIRONMENT to the user mode stack, the initial
alignment of the stack was potentially broken. This caused all sorts of
havoc on vararg functions.

include/minoca/kernel/ps.h
@@ -442,6 +442,7 @@ Author:
 #define DEFAULT_USER_STACK_SIZE (8 * _1MB)
 #define DEFAULT_KERNEL_STACK_SIZE 0x3000
 #define DEFAULT_KERNEL_STACK_SIZE_ALIGNMENT 0x4000
+#define STACK_ALIGNMENT 16
 #define MAX_PROCESS_NAME 255
 #define PS_ALLOCATION_TAG 0x636F7250 // 'corP'
 #define PS_ACCOUNTANT_ALLOCATION_TAG 0x63417350 // 'cAsP'

kernel/ps/env.c
@@ -196,7 +196,7 @@ Return Value:
 
         NewEnvironment = DestinationThread->UserStack +
                          DestinationThread->UserStackSize -
-                         AllocationSize;
+                         ALIGN_RANGE_UP(AllocationSize, STACK_ALIGNMENT);
 
         //
         // Set the thread parameter to point at the environment.