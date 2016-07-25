41f6b57e37201b6a1d649287524695880548960c
Evan Green <evan@minocacorp.com>
2016-07-25 13:20:39 -0700

    Add old err.h functions to C library for tmux.
    Tmux needs the older err.h functions. They're pretty small and isolated.

---
+++ apps/include/libc/err.h
@@ -0,0 +1,286 @@
+/*++
+
+Copyright (c) 2016 Minoca Corp. All Rights Reserved
+
+Module Name:
+
+    err.h
+
+Abstract:
+
+    This header contains definitions for the old err/warn functions.
+
+Author:
+
+    Evan Green 25-Jul-2016
+
+--*/
+
+#ifndef _ERR_H
+#define _ERR_H
+
+//
+// ------------------------------------------------------------------- Includes
+//
+
+#include <libcbase.h>
+#include <stdarg.h>
+#include <stddef.h>
+
+//
+// ---------------------------------------------------------------- Definitions
+//
+
+#ifdef __cplusplus
+
+extern "C" {
+
+#endif
+
+//
+// ------------------------------------------------------ Data Type Definitions
+//
+
+//
+// -------------------------------------------------------------------- Globals
+//
+
+//
+// -------------------------------------------------------- Function Prototypes
+//
+
+LIBC_API
+void
+err (
+    int ExitCode,
+    const char *Format,
+    ...
+    );
+
+/*++
+
+Routine Description:
+
+    This routine prints the program name, the given printf-style formatted
+    string, and the string of the current errno, separated by a colon and a
+    space. This routine then exits the current program with the given exit
+    value.
+
+Arguments:
+
+    ExitCode - Supplies the value to pass to exit.
+
+    Format - Supplies the printf style format string.
+
+    ... - Supplies the remaining arguments to the string.
+
+Return Value:
+
+    This routine does not return, as it calls exit.
+
+--*/
+
+LIBC_API
+void
+verr (
+    int ExitCode,
+    const char *Format,
+    va_list Arguments
+    );
+
+/*++
+
+Routine Description:
+
+    This routine prints the program name, the given printf-style formatted
+    string, and the string of the current errno, separated by a colon and a
+    space. This routine then exits the current program with the given exit
+    value.
+
+Arguments:
+
+    ExitCode - Supplies the value to pass to exit.
+
+    Format - Supplies the printf style format string.
+
+    Arguments - Supplies the remaining arguments to the string.
+
+Return Value:
+
+    This routine does not return, as it calls exit.
+
+--*/
+
+LIBC_API
+void
+errx (
+    int ExitCode,
+    const char *Format,
+    ...
+    );
+
+/*++
+
+Routine Description:
+
+    This routine prints the program name, and the given printf-style formatted
+    string, separated by a colon and a space. This routine then exits the
+    current program with the given exit value.
+
+Arguments:
+
+    ExitCode - Supplies the value to pass to exit.
+
+    Format - Supplies the printf style format string.
+
+    ... - Supplies the remaining arguments to the string.
+
+Return Value:
+
+    This routine does not return, as it calls exit.
+
+--*/
+
+LIBC_API
+void
+verrx (
+    int ExitCode,
+    const char *Format,
+    va_list Arguments
+    );
+
+/*++
+
+Routine Description:
+
+    This routine prints the program name, and the given printf-style formatted
+    string, separated by a colon and a space. This routine then exits the
+    current program with the given exit value.
+
+Arguments:
+
+    ExitCode - Supplies the value to pass to exit.
+
+    Format - Supplies the printf style format string.
+
+    Arguments - Supplies the remaining arguments to the string.
+
+Return Value:
+
+    This routine does not return, as it calls exit.
+
+--*/
+
+LIBC_API
+void
+warn (
+    const char *Format,
+    ...
+    );
+
+/*++
+
+Routine Description:
+
+    This routine prints the program name, the given printf-style formatted
+    string, and the string of the current errno, separated by a colon and a
+    space.
+
+Arguments:
+
+    Format - Supplies the printf style format string.
+
+    ... - Supplies the remaining arguments to the string.
+
+Return Value:
+
+    None.
+
+--*/
+
+LIBC_API
+void
+vwarn (
+    const char *Format,
+    va_list Arguments
+    );
+
+/*++
+
+Routine Description:
+
+    This routine prints the program name, the given printf-style formatted
+    string, and the string of the current errno, separated by a colon and a
+    space.
+
+Arguments:
+
+    Format - Supplies the printf style format string.
+
+    Arguments - Supplies the remaining arguments to the string.
+
+Return Value:
+
+    None.
+
+--*/
+
+LIBC_API
+void
+warnx (
+    const char *Format,
+    ...
+    );
+
+/*++
+
+Routine Description:
+
+    This routine prints the program name, and the given printf-style formatted
+    string, separated by a colon and a space.
+
+Arguments:
+
+    Format - Supplies the printf style format string.
+
+    ... - Supplies the remaining arguments to the string.
+
+Return Value:
+
+    None.
+
+--*/
+
+LIBC_API
+void
+vwarnx (
+    const char *Format,
+    va_list Arguments
+    );
+
+/*++
+
+Routine Description:
+
+    This routine prints the program name, and the given printf-style formatted
+    string, separated by a colon and a space.
+
+Arguments:
+
+    Format - Supplies the printf style format string.
+
+    Arguments - Supplies the remaining arguments to the string.
+
+Return Value:
+
+    None.
+
+--*/
+
+#ifdef __cplusplus
+
+}
+
+#endif
+#endif
+
---
+++ apps/libc/dynamic/err.c
@@ -0,0 +1,385 @@
+/*++
+
+Copyright (c) 2016 Minoca Corp. All Rights Reserved
+
+Module Name:
+
+    err.c
+
+Abstract:
+
+    This module implements support for the old err/warn functions.
+
+Author:
+
+    Evan Green 25-Jul-2016
+
+Environment:
+
+    User Mode C Library
+
+--*/
+
+//
+// ------------------------------------------------------------------- Includes
+//
+
+#include "libcp.h"
+#include <err.h>
+#include <errno.h>
+#include <stdio.h>
+
+//
+// ---------------------------------------------------------------- Definitions
+//
+
+//
+// ------------------------------------------------------ Data Type Definitions
+//
+
+//
+// ----------------------------------------------- Internal Function Prototypes
+//
+
+//
+// -------------------------------------------------------------------- Globals
+//
+
+//
+// ------------------------------------------------------------------ Functions
+//
+
+LIBC_API
+void
+err (
+    int ExitCode,
+    const char *Format,
+    ...
+    )
+
+/*++
+
+Routine Description:
+
+    This routine prints the program name, the given printf-style formatted
+    string, and the string of the current errno, separated by a colon and a
+    space. This routine then exits the current program with the given exit
+    value.
+
+Arguments:
+
+    ExitCode - Supplies the value to pass to exit.
+
+    Format - Supplies the printf style format string.
+
+    ... - Supplies the remaining arguments to the string.
+
+Return Value:
+
+    This routine does not return, as it calls exit.
+
+--*/
+
+{
+
+    va_list Arguments;
+
+    va_start(Arguments, Format);
+    verr(ExitCode, Format, Arguments);
+    va_end(Arguments);
+    return;
+}
+
+LIBC_API
+void
+verr (
+    int ExitCode,
+    const char *Format,
+    va_list Arguments
+    )
+
+/*++
+
+Routine Description:
+
+    This routine prints the program name, the given printf-style formatted
+    string, and the string of the current errno, separated by a colon and a
+    space. This routine then exits the current program with the given exit
+    value.
+
+Arguments:
+
+    ExitCode - Supplies the value to pass to exit.
+
+    Format - Supplies the printf style format string.
+
+    Arguments - Supplies the remaining arguments to the string.
+
+Return Value:
+
+    This routine does not return, as it calls exit.
+
+--*/
+
+{
+
+    PPROCESS_ENVIRONMENT Environment;
+
+    Environment = OsGetCurrentEnvironment();
+    fprintf(stderr, "%s: ", Environment->Arguments[0]);
+    if (Format != NULL) {
+        vfprintf(stderr, Format, Arguments);
+        fprintf(stderr, ": ");
+    }
+
+    fprintf(stderr, "%s\n", strerror(errno));
+    exit(ExitCode);
+    return;
+}
+
+LIBC_API
+void
+errx (
+    int ExitCode,
+    const char *Format,
+    ...
+    )
+
+/*++
+
+Routine Description:
+
+    This routine prints the program name, and the given printf-style formatted
+    string, separated by a colon and a space. This routine then exits the
+    current program with the given exit value.
+
+Arguments:
+
+    ExitCode - Supplies the value to pass to exit.
+
+    Format - Supplies the printf style format string.
+
+    ... - Supplies the remaining arguments to the string.
+
+Return Value:
+
+    This routine does not return, as it calls exit.
+
+--*/
+
+{
+
+    va_list Arguments;
+
+    va_start(Arguments, Format);
+    verrx(ExitCode, Format, Arguments);
+    va_end(Arguments);
+    return;
+}
+
+LIBC_API
+void
+verrx (
+    int ExitCode,
+    const char *Format,
+    va_list Arguments
+    )
+
+/*++
+
+Routine Description:
+
+    This routine prints the program name, and the given printf-style formatted
+    string, separated by a colon and a space. This routine then exits the
+    current program with the given exit value.
+
+Arguments:
+
+    ExitCode - Supplies the value to pass to exit.
+
+    Format - Supplies the printf style format string.
+
+    Arguments - Supplies the remaining arguments to the string.
+
+Return Value:
+
+    This routine does not return, as it calls exit.
+
+--*/
+
+{
+
+    PPROCESS_ENVIRONMENT Environment;
+
+    Environment = OsGetCurrentEnvironment();
+    fprintf(stderr, "%s: ", Environment->Arguments[0]);
+    if (Format != NULL) {
+        vfprintf(stderr, Format, Arguments);
+        fprintf(stderr, ": ");
+    }
+
+    fprintf(stderr, "\n");
+    exit(ExitCode);
+    return;
+}
+
+LIBC_API
+void
+warn (
+    const char *Format,
+    ...
+    )
+
+/*++
+
+Routine Description:
+
+    This routine prints the program name, the given printf-style formatted
+    string, and the string of the current errno, separated by a colon and a
+    space.
+
+Arguments:
+
+    Format - Supplies the printf style format string.
+
+    ... - Supplies the remaining arguments to the string.
+
+Return Value:
+
+    None.
+
+--*/
+
+{
+
+    va_list Arguments;
+
+    va_start(Arguments, Format);
+    vwarn(Format, Arguments);
+    va_end(Arguments);
+    return;
+}
+
+LIBC_API
+void
+vwarn (
+    const char *Format,
+    va_list Arguments
+    )
+
+/*++
+
+Routine Description:
+
+    This routine prints the program name, the given printf-style formatted
+    string, and the string of the current errno, separated by a colon and a
+    space.
+
+Arguments:
+
+    Format - Supplies the printf style format string.
+
+    Arguments - Supplies the remaining arguments to the string.
+
+Return Value:
+
+    None.
+
+--*/
+
+{
+
+    PPROCESS_ENVIRONMENT Environment;
+
+    Environment = OsGetCurrentEnvironment();
+    fprintf(stderr, "%s: ", Environment->Arguments[0]);
+    if (Format != NULL) {
+        vfprintf(stderr, Format, Arguments);
+        fprintf(stderr, ": ");
+    }
+
+    fprintf(stderr, "%s\n", strerror(errno));
+    return;
+}
+
+LIBC_API
+void
+warnx (
+    const char *Format,
+    ...
+    )
+
+/*++
+
+Routine Description:
+
+    This routine prints the program name, and the given printf-style formatted
+    string, separated by a colon and a space.
+
+Arguments:
+
+    Format - Supplies the printf style format string.
+
+    ... - Supplies the remaining arguments to the string.
+
+Return Value:
+
+    None.
+
+--*/
+
+{
+
+    va_list Arguments;
+
+    va_start(Arguments, Format);
+    vwarnx(Format, Arguments);
+    va_end(Arguments);
+    return;
+}
+
+LIBC_API
+void
+vwarnx (
+    const char *Format,
+    va_list Arguments
+    )
+
+/*++
+
+Routine Description:
+
+    This routine prints the program name, and the given printf-style formatted
+    string, separated by a colon and a space.
+
+Arguments:
+
+    Format - Supplies the printf style format string.
+
+    Arguments - Supplies the remaining arguments to the string.
+
+Return Value:
+
+    None.
+
+--*/
+
+{
+
+    PPROCESS_ENVIRONMENT Environment;
+
+    Environment = OsGetCurrentEnvironment();
+    fprintf(stderr, "%s: ", Environment->Arguments[0]);
+    if (Format != NULL) {
+        vfprintf(stderr, Format, Arguments);
+        fprintf(stderr, ": ");
+    }
+
+    fprintf(stderr, "\n");
+    return;
+}
+
+//
+// --------------------------------------------------------- Internal Functions
+//
+
