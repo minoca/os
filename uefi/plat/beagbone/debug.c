0373dd7765689aa2f5fd7c1fcdb8d78c24f89e6b
Evan Green <evan@minocacorp.com>
2016-10-25 11:42:41 -0700

    Add GPLv3 banner to all .c, .h, .S, and .ck files.
    Here we go...

--- uefi/plat/beagbone/debug.c
+++ uefi/plat/beagbone/debug.c
@@ -1,6 +1,11 @@
 /*++
 
-Copyright (c) 2014 Minoca Corp. All Rights Reserved
+Copyright (c) 2014 Minoca Corp.
+
+    This file is licensed under the terms of the GNU General Public License
+    version 3. Alternative licensing terms are available. Contact
+    info@minocacorp.com for details. See the LICENSE file at the root of this
+    project for complete licensing information.
 
 Module Name:
 
