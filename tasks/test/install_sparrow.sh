436be1c935f334e98878324cfcd25561e3a35b78
Alexey Melezhik <melezhik@gmail.com>
2016-12-03 22:16:15 +0300

    add minoca-pkg-test run to automation process

---
+++ tasks/test/install_sparrow.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+##
+## Script Name:
+##
+##     install_sparrow.sh
+##
+## Abstract:
+##
+##     This script installs Sparrow tool on running Minoca OS.
+##     Installation script takes several steps:
+##     1) Installs sparrow system prerequisites (bash and curl), 
+##     some system prerequisites ( like make, gcc so on ) are to be installed _earlier_
+##     see os/tasks/build/add_files.sh file
+##     2) Installs cpanminus client to install CPAN modules
+##     3) Install sparrow tool as CPAN module
+##
+## Author:
+##
+##     Alexey Melezhik 06-Dec-2016
+##
+##
+
+set -e
+set -x
+
+opkg -f /etc/opkg/opkg.conf update
+opkg -f /etc/opkg/opkg.conf  install bash curl
+curl -k -L https://cpanmin.us -o /usr/bin/cpanm
+chmod +x /usr/bin/cpanm
+ln -fs /bin/env /usr/bin/env
+cpanm --notest -q Sparrow
+sparrow index update
