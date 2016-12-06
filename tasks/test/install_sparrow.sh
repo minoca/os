1707e68775ce54976ab83dcecfd10f564ddff3f1
Alexey Melezhik <melezhik@gmail.com>
2016-12-06 12:03:28 +0000

    fix typo. cpan -> cpanm

--- tasks/test/install_sparrow.sh
+++ tasks/test/install_sparrow.sh
@@ -25,7 +25,7 @@ set -x
 
 opkg -f /etc/opkg/opkg.conf update
 opkg -f /etc/opkg/opkg.conf  install bash curl
-curl -k -L https://cpanmin.us -o /usr/bin/cpan
+curl -k -L https://cpanmin.us -o /usr/bin/cpanm
 chmod +x /usr/bin/cpanm
 ln -fs /bin/env /usr/bin/env
 cpanm --notest -q Sparrow
