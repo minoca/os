f9677f70ed2ba7a3301ee9a4d65068de595f4f83
Evan Green <evan@minocacorp.com>
2017-02-23 18:21:30 -0800

    Remove IMAGE_SECTION_CACHEABLE from copy mask.
    The IMAGE_SECTION_CACHEABLE is internal and derived when an image section
    is created. It is an error to pass it in during copy, and causes an assert
    to fire (that none of the internal flags are being set).

--- include/minoca/kernel/mm.h
+++ include/minoca/kernel/mm.h
@@ -136,7 +136,7 @@ Author:
 #define IMAGE_SECTION_COPY_MASK                             \
     (IMAGE_SECTION_ACCESS_MASK | IMAGE_SECTION_NON_PAGED |  \
      IMAGE_SECTION_SHARED | IMAGE_SECTION_MAP_SYSTEM_CALL | \
-     IMAGE_SECTION_WAS_WRITABLE | IMAGE_SECTION_CACHEABLE)
+     IMAGE_SECTION_WAS_WRITABLE)
 
 //
 // Define a mask of image section access flags.
