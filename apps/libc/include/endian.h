91d05de16505d11a1e1e38f11c1b4f2f71f4b9a2
Evan Green <evan@minocacorp.com>
2017-03-24 11:50:25 -0700

    Added some older BSD byte swapping macros to endian.h.
    xcb-util-cursor was using one of these macros. Right now they use
    __builtin_bswapXX functions. It's a little GCC specific, but should be
    easy enough to replace if we port to a different compiler. I think clang
    has these macros as well. __builtin_bswap16 was only added to x86 in
    GCC 4.8. As of this date, we're using 6.3.0.

--- apps/libc/include/endian.h
+++ apps/libc/include/endian.h
@@ -42,6 +42,51 @@ Author:
 #define BIG_ENDIAN 4321
 
 //
+// Define the byteswapping macros. For now, use the builtin, though this is
+// a little compiler-specific.
+//
+
+#define __byteswap16(_Value) __builtin_bswap16(_Value)
+#define __byteswap32(_Value) __builtin_bswap32(_Value)
+#define __byteswap64(_Value) __builtin_bswap64(_Value)
+
+#if BYTE_ORDER == LITTLE_ENDIAN
+
+#define htobe16(_Value) __byteswap16(_Value)
+#define htole16(_Value) (_Value)
+#define be16toh(_Value) __byteswap16(_Value)
+#define le16toh(_Value) (_Value)
+
+#define htobe32(_Value) __byteswap32(_Value)
+#define htole32(_Value) (_Value)
+#define be32toh(_Value) __byteswap32(_Value)
+#define le32toh(_Value) (_Value)
+
+#define htobe64(_Value) __byteswap64(_Value)
+#define htole64(_Value) (_Value)
+#define be64toh(_Value) __byteswap64(_Value)
+#define le64toh(_Value) (_Value)
+
+#else
+
+#define htobe16(_Value) (_Value)
+#define htole16(_Value) __byteswap16(_Value)
+#define be16toh(_Value) (_Value)
+#define le16toh(_Value) __byteswap16(_Value)
+
+#define htobe32(_Value) (_Value)
+#define htole32(_Value) __byteswap32(_Value)
+#define be32toh(_Value) (_Value)
+#define le32toh(_Value) __byteswap32(_Value)
+
+#define htobe64(_Value) (_Value)
+#define htole64(_Value) __byteswap64(_Value)
+#define be64toh(_Value) (_Value)
+#define le64toh(_Value) __byteswap64(_Value)
+
+#endif
+
+//
 // ------------------------------------------------------ Data Type Definitions
 //
 
