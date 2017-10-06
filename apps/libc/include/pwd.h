1f3cdf58bcaaf0cf3378cef962e0c40e5deec72a
Evan Green <evan@minocacorp.com>
2017-10-06 10:37:18 -0700

    Relicense the C library headers under LGPLv3.
    This change relicenses the C library headers under the LGPLv3, so that
    folks creating regular portable applications don't have to worry that by
    including Minoca's C library headers they're subject to the GPL.

--- apps/libc/include/pwd.h
+++ apps/libc/include/pwd.h
@@ -2,10 +2,9 @@
 
 Copyright (c) 2013 Minoca Corp.
 
-    This file is licensed under the terms of the GNU General Public License
-    version 3. Alternative licensing terms are available. Contact
-    info@minocacorp.com for details. See the LICENSE file at the root of this
-    project for complete licensing information.
+    This file is licensed under the terms of the GNU Lesser General Public
+    License version 3. Alternative licensing terms are available. Contact
+    info@minocacorp.com for details.
 
 Module Name:
 
